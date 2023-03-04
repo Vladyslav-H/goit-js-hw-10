5c9243612e98b814fde144a12dc8b8989773bd4a
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-04 11:59:31 +0200

    Refactoring code 10-task.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
- export  function fetchCountries (countryName) {
+export function fetchCountries(countryName) {
   const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
-     return fetch(url).then(response => {
-         if (!response.ok) {
-             throw new Error(response.status)
-         }
-             return response.json();
-         }
-     );
+
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
@@ -8,16 +9,20 @@ const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
+
 inputEl.addEventListener('input', debounce(onInputName, DEBOUNCE_DELAY));
 
 function onInputName(event) {
   event.preventDefault();
   const countryName = event.target.value.trim();
+
   if (countryName === '') {
     countryListEl.innerHTML = '';
     countryInfoEl.innerHTML = '';
+
     return;
   }
+
   fetchCountries(countryName).then(checkQuantityOfCountries).catch(fail);
 }
 
@@ -26,9 +31,11 @@ function checkQuantityOfCountries(array) {
     Notify.info('Too many matches found. Please enter a more specific name.');
   } else if (array.length >= 2 && array.length <= 10) {
     countryInfoEl.innerHTML = '';
+
     createCountriesList(array);
   } else if (array.length === 1) {
     countryListEl.innerHTML = '';
+
     createCountryCard(array);
   }
 }
@@ -36,6 +43,7 @@ function checkQuantityOfCountries(array) {
 function createCountryCard(array) {
   const countryObj = array[0];
   const { name, capital, population, flags, languages } = countryObj;
+
   const coutryCard = `<ul class='country-info-card nolist'>
                 <li class='country-info-item'><img src ='${flags.svg}'
                  alt ='flag of ${name.official}' width ='40' height ='30'/>    
@@ -46,26 +54,34 @@ function createCountryCard(array) {
                   languages
                 )}</li>
                 </ul>`;
+
   addCountryCardToHTML(coutryCard);
 }
 
 function createCountriesList(array) {
   let countryList = '';
-  for (let { flags, name } of array) {
-    countryList += `<li class='country-list-item'><img src='${flags.svg}' 
-        alt='flag of ${name.official}'width ='30' height ='20'/><p> ${name.official}</p></li>`;
-  }
+
+  array.map(
+    ({ flags, name }) =>
+      (countryList += `<li class='country-list-item'>
+      <img src='${flags.svg}'alt='flag of ${name.official}'width ='30' height ='20'/>
+        <p> ${name.official}</p></li>`)
+  );
+
   addCountriesListToHTML(countryList);
 }
 
 function addCountryCardToHTML(element) {
   countryInfoEl.innerHTML = element;
 }
+
 function addCountriesListToHTML(element) {
   countryListEl.innerHTML = element;
 }
+
 function fail() {
   countryListEl.innerHTML = '';
   countryInfoEl.innerHTML = '';
+
   Notify.failure('Oops, there is no country with that name');
 }
